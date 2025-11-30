a78a7d9840e1b6a318cfecfcd428f3889ad73c87
twingall1 <34135758+twingall1@users.noreply.github.com>
2025-11-30 08:02:19 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1176,22 +1176,22 @@ function renderSingleVault(lock) {
       <div class="vault-body">
         <div class="vault-col-main">
           <div class="col1-line">
-            target&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+            target&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;💲:
             &nbsp;<span class="col1-value-bold">1 ${assetLabel} ≥ $${formatLockPrice(thresholdFloat)}</span>
           </div>
         
           <div class="col1-line">
-            current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+            current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;💲:
             &nbsp;<span class="col1-value-bold">$${formatLockPrice(currentPriceFloat)}</span>
           </div>
         
           <div class="col1-line">
-            time&nbsp;unlock&nbsp;:
+            unlock&nbsp;⏱️:
             &nbsp;<span class="col1-value-bold">${formatTimestamp(lock.unlockTime)}</span>
           </div>
         
           <div class="col1-line">
-            locked&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+            locked&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;🪙:
             &nbsp;<span class="col1-value-bold">${balanceFloat.toFixed(4)} ${assetLabel}</span>
           </div>
         </div>
